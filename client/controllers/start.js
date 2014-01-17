ff3be5e7995695ea5b376da923d435b9d72bcb9e
Spencer Sims <git@spencersims.com>
2014-01-16 18:34:50 -0800

    filter created to sort likes and comments

--- client/controllers/start.js
+++ client/controllers/start.js
@@ -9,4 +9,87 @@ app.controller('start',['$scope','$location','info', function ($scope,$location,
 		});
 	};
 	getData();
+	
+	$scope.model = {filter:{likes:false,comments:false},filterAmount:{likes:{min:0,max:0},comments:{min:0,max:0}}};
+
+//<!--ng-if="post.likes.count>=model.filterAmount.likes.min && post.likes.count<=model.filterAmount.likes.max" !-->
+	$scope.all = function(post){
+		var likes = post.likes==undefined ? 0 : post.likes.count; 
+		var comments = post.comments==undefined ? 0 : post.comments.count; 
+
+		var minLikes = $scope.model.filterAmount.likes.min;
+		var maxLikes = $scope.model.filterAmount.likes.max;
+		var minComments = $scope.model.filterAmount.comments.min;
+		var maxComments = $scope.model.filterAmount.comments.max;
+	
+		var filter=false;
+		if(minLikes==maxLikes && minComments==maxComments){
+			return post;
+		}
+
+
+		if((likes>=minLikes && likes<=maxLikes) && minComments+maxComments==0){
+			filter=true;
+		}
+		if((comments>=minComments && comments<=maxComments) &&  minLikes+maxLikes==0){
+			filter=true;
+		}
+		if(filter){
+			return post;
+		}else{
+			if(likes>=minLikes && likes<=maxLikes){
+				if(comments>=minComments && comments<=maxComments){
+					return post;
+				}
+			}
+		}
+		
+	};
+	$scope.likes = function(post){
+		var likes = post.likes==undefined ? 0 : post.likes.count; 
+
+
+		var minLikes = $scope.model.filterAmount.likes.min;
+		var maxLikes = $scope.model.filterAmount.likes.max;
+	
+		if(minLikes==maxLikes){
+			return post;
+		}
+
+
+		if((likes>=minLikes && likes<=maxLikes)){
+			return post;
+		}
+
+		
+	};
+	$scope.comments = function(post){
+		var comments = post.comments==undefined ? 0 : post.comments.count; 
+
+
+		var minComments = $scope.model.filterAmount.comments.min;
+		var maxComments = $scope.model.filterAmount.comments.max;
+	
+		if(minComments==maxComments){
+			return post;
+		}
+
+
+		if((comments>=minComments && comments<=maxComments)){
+			return post;
+		}
+
+		
+	};
 }]);
+
+app.filter('likeSort', function() {
+	var functions = {};
+	functions = {
+		main:function(data) {
+			return data;
+		}
+	};
+
+	return functions.main
+});
