d827049033e9c96ac38ee940ba39b512fe6b6e53
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-04-05 10:46:03 -0400

    first commit

---
+++ citizennet/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Citizennet::Application.config.secret_key_base = '8aea0aea0b7941176c2682aa1c5e2407bbaaf13c6ce9c54da39847a2c66b4bd3d29439045fdfb732fe6cd4cc9ca04755b0f0d44981872030e9dc8dd1be8c5859'
