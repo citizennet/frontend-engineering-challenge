cb7f46fc6644daf5e3771f18fc328709aec1fea6
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-04-10 15:19:19 -0400

    deletions

--- citizennet/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Citizennet::Application.routes.draw do
-  root 'posts#index'
-  resources :posts, only: [:index]
-end
--- citizennet/spec/features/user_sees_posts.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../spec_helper.rb'
-
-describe 'user sees posts' do 
-
-  it "shows a list of recent posts (which are stored in cache memory)" do 
-    visit '/'
-    expect(page).to have_css('#posts')
-  end
-end
