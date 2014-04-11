49344e0e38937ffd848996f9308d715cadd21c69
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-04-10 21:23:20 -0400

    added description

--- code_challenge/app/assets/javascripts/application.js
+++ code_challenge/app/assets/javascripts/application.js
@@ -18,26 +18,32 @@
 var posts;
 
 function getPosts() {
+  $("<div id='posts'></div>").appendTo('#contents');
   $.getJSON('/posts', function(response) {
     posts = response;
   }).done(function() {
     appendPosts(posts);
+  }).done(function() {
+    loadListeners();
   })
 }
 function appendPosts(posts) {
   for(var i = 0; i < posts.data.length; i++) {
     if(posts.data[i].description !== undefined){
-      $("<div class='post' id='post-"+i+"'><div id='name'><h3>"+posts.data[i].name+"</h3></div></div>").appendTo('#posts');
+      $("<div class='post' id='post-"+i+"'><div id='name'><a href='"+posts.data[i].link+"'><h3>"+posts.data[i].name+"</h3></a></div></div></br>").appendTo('#posts');
     }
   }
 }
 
-$( document ).ready(function() {
-  getPosts()
-  $("<div id='posts'></div>").appendTo('#contents');
-  $('div').on("mouseover", function(e) {
-    if(e.target.class === 'post') {
-      console.log('hi');
-    }
+function loadListeners() {
+  $('h3').mouseenter(function(e) {
+    $('.post-hover').remove();
+    var post = $(e.target).parent().parent().parent()
+    var postID = post[0].id.split('-')[1]
+    $(this).parent().parent().append("<div class='post-hover'>"+posts.data[postID]+"<p>"+posts.data[postID].description<br?+"</p></div>")
   })
+}
+
+$( document ).ready(function() {
+  getPosts();
 }) // end doc ready
