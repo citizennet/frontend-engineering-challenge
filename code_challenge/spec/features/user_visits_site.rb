6e5c161ad8191d89b53825a8016b525b09e80518
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-04-16 09:17:21 -0700

    tests

--- code_challenge/spec/features/user_visits_site.rb
+++ code_challenge/spec/features/user_visits_site.rb
@@ -1,9 +1,12 @@
 require_relative '../spec_helper'
 
-describe 'a user visits the home page' do 
-  it "should display posts" do 
+describe 'a user visits the home page'do 
+  it "should display posts", :js => true do 
     visit '/'
     expect(page).to have_css('#posts')
+  end
+  it "should display likes", :js => true do 
+    visit '/'
     expect(page).to have_css('#likes')
   end
 end
