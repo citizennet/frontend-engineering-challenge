a2a9c6ff0b43b4d3143792ff53db827ee64f7c4f
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-04-16 09:39:28 -0700

    added link to likes

--- code_challenge/app/assets/javascripts/application.js
+++ code_challenge/app/assets/javascripts/application.js
@@ -16,10 +16,10 @@
 //= require_tree .
 
 var posts;
-
+var likes;
+//adds posts to page
 function appendPosts(posts) {
   for(var i = 0; i < posts.data.length; i++) {
-    //adds posts to page
     if(posts.data[i].description !== undefined){
       $("<div class='post' id='post-"+i+"'><div id='name'><a href='"+posts.data[i].link+"'><h3>"+posts.data[i].name+"</h3></a></div></div></br>").appendTo('#posts');
     }
@@ -28,10 +28,10 @@ function appendPosts(posts) {
 
 function appendLikes(likes) {
   // adds likes to page
-  // what to do with the likes was the most unclear part of the assignment for me
+  // what to do with the likes was the most ambiguous part of the assignment for me
   for(var i = 0; i < likes.data.length; i++) {
-    var name = likes.data[i].name
-    $("<div class='like'>"+name+': '+likes.data[i].category+"</div>").appendTo('#likes');
+    var link = "<a href='http://google.com/#q="+likes.data[i].name.replace(" ", "+")+"'>"+likes.data[i].name+"</a>";
+    $("<div class='like'>"+link+': '+likes.data[i].category+"</div>").appendTo('#likes');
   }
 }
 
@@ -41,16 +41,13 @@ function loadListeners() {
     $('.post-hover').remove();
     var post = $(e.target).parent().parent().parent();
     var postID = post[0].id.split('-')[1];
-    // var message = posts.data[postID].message.split('http')[0]
-    // var description = posts.data[postID].description
-    // var picture = posts.data[postID].picture
     link = posts.data[postID].link
     // using iframe eventually seemed to be the most effective way to view article data
     // however it doesn't work for every single post...
     $("<iframe src='"+link+"' class='post-hover'></iframe>+").fadeIn(1000).appendTo('#article-space');
   })
 }
-
+//makes an AJAX request for post data, then runs append posts and loads event listeners
 function getPosts() {
   $("<div id='posts'><h5 id='recent-posts'>Recent Posts:</h5></br></div>").appendTo('#contents');
   $.getJSON('/posts', function(response) {
@@ -61,9 +58,7 @@ function getPosts() {
     loadListeners();
   })
 }
-
-
-
+//makes an AJAX request for like data
 function getLikes() {
   $("<div id='likes'><h5 id='recent-likes'>Likes: </h5></div>").appendTo('#contents');
   $.getJSON('/likes', function(response) {
--- code_challenge/spec/spec_helper.rb
+++ code_challenge/spec/spec_helper.rb
@@ -6,8 +6,6 @@ require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/webkit/matchers'
 Capybara.javascript_driver = :webkit
-require 'simplecov'
-SimpleCov.start
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
